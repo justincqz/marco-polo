e963799fb29a666ffd1212a005394fffc01d3100
qzc17 <qzc17@ic.ac.uk>
2018-01-28 01:54:47 +0000

    Implemented JSON load file

--- controllers/serialiseJSON.js
+++ controllers/serialiseJSON.js
@@ -10,3 +10,13 @@ exports.saveFile = (req, res) => {
       res.send("Saved");
   });
 };
+
+exports.loadFile = (req, res) => {
+  var uniqueId = req.query.id;
+  fs.readFile('/etc/'+uniqueId, (err, data) => {
+  if (err) {
+    res.send(err);
+  }
+    res.send(data);
+  });
+}
