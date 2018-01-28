43a07f1a27feb79d2cf0a1056c4c85a7e5431f22
qzc17 <qzc17@ic.ac.uk>
2018-01-28 06:04:20 +0000

    Added downvote

--- controllers/serialiseJSON.js
+++ controllers/serialiseJSON.js
@@ -74,6 +74,32 @@ exports.updateFile = (req, res) => {
     console.log(updateData);
     res.send(updateData);
   });
+}
 
-
+exports.downvoteFile = (req, res) => {
+  var uniqueId = req.body.id;
+  fs.readFile("./db/"+uniqueId, 'utf-8', (err, oldData) => {
+  if (err) {
+    res.send(err);
+  }
+    var updateData = JSON.parse(oldData);
+    var newFlights = req.body.data.flights;
+    for (var i = 0; i < newFlights.length; i++){
+      updateData.flights[newFlights[i]].votes -= 1;
+    }
+    var newAccommodations = req.body.data.accommodations;
+    for (var i = 0; i < newAccommodations.length; i++){
+      updateData.accommodations[newAccommodations[i]].votes -= 1;
+    }
+    var newTodos = req.body.data.todos;
+    for (var i = 0; i < newTodos; i++){
+      updateData.todos[newTodos[i]].votes -= 1;
+    }
+    var newEvents = req.body.data.events;
+    for (var i = 0; i < newEvents; i++){
+      updateData.events[newEvents[i]].vote -= 1;
+    }
+    console.log(updateData);
+    res.send(updateData);
+  });
 }
