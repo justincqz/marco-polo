ae599cb9c34bfaf800ca033abf60a05acbef86cf
qzc17 <qzc17@ic.ac.uk>
2018-01-28 02:48:11 +0000

    Rewriting shit

--- controllers/thingstodo.js
+++ controllers/thingstodo.js
@@ -1,56 +1,22 @@
+const Client = require('node-rest-client').Client;
+const client = new Client();
 const GOOGLE_API_KEY = 'AIzaSyDx7O2y6ts1OPlnl9aux7lAAEoLAHAanY4';
-createRequest = function(cityName){
-  var options = {
-      host: 'maps.googleapis.com/',
-      port: 443,
-      path: '/maps/api/place/textsearch/json',
-      method: 'GET',
-      headers: {
-          'Content-Type': 'application/json',
-          'query' : 'Things+to+do+in+' + cityName,
-          'key' : GOOGLE_API_KEY
-      }
-  };
-  return options;
-}
-//Set Options
 
 exports.getThingsToDo = (req, res) => {
-  var currentCity = req.body.destCity;
-  rest.getJSON(createRequest(currentCity), function(statusCode, result) {
-      // I could work with the result html/json here.  I could also just return it
-      //console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
-      res.statusCode = statusCode;
-      res.send(result);
+  var currentCity = req.query.destCity;
+  var queryString = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=Things+to+do+in+' + currentCity+ '&key=' + GOOGLE_API_KEY;
+
+  var r = [];
+  client.get(queryString, '', function (data, response) {
+    var destinations = data.results;
+    //console.log(data.results);
+
+    for (var i = 0; i < destinations.length; i++){
+      r.push({
+        name: destinations[i].name,
+        location: destinations[i].formatted_address,
+      })
+    }
+    res.json(r);
   });
-};
-
-
-//get Json function
-getJSON = function(options, onResult)
-{
-    //console.log("rest::getJSON");
-
-    var port = options.port == 443 ? https : http;
-    var req = port.request(options, function(res)
-    {
-        var output = '';
-        c//onsole.log(options.host + ':' + res.statusCode);
-        res.setEncoding('utf8');
-
-        res.on('data', function (chunk) {
-            output += chunk;
-        });
-
-        res.on('end', function() {
-            var obj = JSON.parse(output);
-            onResult(res.statusCode, obj);
-        });
-    });
-
-    req.on('error', function(err) {
-        //res.send('error: ' + err.message);
-    });
-
-    req.end();
 };
